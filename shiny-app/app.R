cdaad624b79c396f4f33eb68aaad72b89b22d611
William Eng <1williameng@gmail.com>
2023-03-05 23:10:57 -0800

    shiny app!

---
+++ shiny-app/app.R
@@ -0,0 +1,122 @@
+library(shiny)
+library(tidyverse)
+
+UAH <- read_delim("UAH-lower-troposphere-long.csv.bz2")
+
+ui <- fluidPage(
+    tabsetPanel(
+      tabPanel("About", 
+        titlePanel("General Information!"),
+        HTML(paste("The data we are examining is the <strong>deviation in temperature</strong>
+        (<em>in degrees celcius</em>) from the average temperatures from 1991-2020.
+        Below are 5 lines of randomly selected data points!")),
+        mainPanel(
+          tableOutput("data")
+        )
+      ),
+      
+      tabPanel("Plots",
+        titlePanel("Temperature Deviations (/year) by Region"),
+        sidebarLayout(
+          sidebarPanel(
+            checkboxGroupInput("regions", "Select regions to display:", 
+                               choices = unique(UAH$region), 
+                               selected = "aust")
+          ),
+          mainPanel(
+            plotOutput("plot"),
+            textOutput("selected")
+          )
+        )
+      ),
+      
+      tabPanel("Table",
+        titlePanel("table of average temperature deviations by region"),
+     
+        sidebarLayout(
+          sidebarPanel(
+           radioButtons("period", "Select Time Period:",
+                      c("month", "year", "deacade"), selected = "deacade"),
+           textOutput("numObservations")
+          ),
+       
+          mainPanel(
+            tableOutput("table")
+          )
+        )
+    )
+  )
+)
+  
+
+server <- function(input, output) {
+  output$data <- renderTable({
+    UAH %>% 
+      sample_n(5)
+  })
+  
+  output$plot <- renderPlot({
+    UAH %>% 
+      filter(region %in% input$regions) %>% 
+      group_by(region) %>% 
+      ggplot(aes(x=year, y=temp, group = region, color=factor(region)))+
+      geom_point()+
+      labs(x="Year", y="Deviation in Temperature (deg C)", color="Region")
+  })
+  
+  output$selected <- renderText({
+    if (UAH %>% 
+      filter(region %in% input$regions) %>%
+      nrow() == 0) {
+      "No regions are selected!!"
+    } else {
+        regionsDisplayed <- UAH %>% 
+        filter(region %in% input$regions) %>%
+        select(region) %>% 
+        distinct() %>% 
+        unlist()
+        paste0("Now Displaying (", 
+               length(regionsDisplayed), " reg): ", 
+               paste0(regionsDisplayed, collapse = ", "))
+    }
+  })
+  
+  output$table <- renderTable({
+    if (input$period == "month") {
+      UAH %>%
+        group_by(year, month) %>%
+        summarize(avg_temp = mean(temp))
+    } else if (input$period == "year") {
+      UAH %>%
+        group_by(year) %>%
+        summarize(avg_temp = mean(temp))
+    } else {
+      UAH %>%
+        group_by(decade = 10*(year %/% 10)) %>%
+        summarize(avg_temp = mean(temp))
+    }
+  })
+  
+  output$numObservations <- renderText({
+    if (input$period == "month") {
+      numObs <- UAH %>%
+        group_by(year, month) %>%
+        summarize(avg_temp = mean(temp)) %>% 
+        nrow()
+    } else if (input$period == "year") {
+      numObs <- UAH %>%
+        group_by(year) %>%
+        summarize(avg_temp = mean(temp)) %>% 
+        nrow()
+    } else {
+      numObs <- UAH %>%
+        group_by(decade = 10*(year %/% 10)) %>%
+        summarize(avg_temp = mean(temp)) %>% 
+        nrow()
+    }
+    paste("Displaying ", numObs, " Observations!")
+  })
+}
+
+
+shinyApp(ui = ui, server = server)
